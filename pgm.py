c1b5336c67022d39df2a633ce3b9b02c378b3bcd
mr-billyu <linux9window0@yahoo.com>
2019-01-26 14:20:15 -0500

    Modified program to use argparse to get the search path from the command line

--- pgm.py
+++ pgm.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+import argparse
 from myTk import myTk
 import os
 import re
 
 
 def get_entry(event='x'):
-    global path
+    global directory
     gui.clearlist('results')
     param = gui.getentry('searchfor')
     option = '-n'
@@ -15,19 +16,25 @@ def get_entry(event='x'):
     if gui.getcheckbutton('word') == 1:
         option = option + 'w'
     option = option + ' '
-    results = os.popen("grep " + option + " " + param + 
-             " /home/pi/ssh/BillyU_data/Bill/Documents/raspberrypi/*.txt")
+    results = os.popen("grep " + option + " " + param + " " +
+             directory + "/*.txt")
     for line in results:
         m = re.match(r'(^\S*/)(\S*.txt:.*)', line)
-        path = m.group(1)
         gui.setlist('results', m.group(2))
     gui.setlist('results', '======END======')
 
 def get_selected(event):
-    global path
+    global directory 
     selection = gui.getlistselection('results').split(":", 2)
-    filename = path + selection[0]
+    filename = directory + selection[0]
     os.system('lxterminal -e less +' + selection[1] + ' ' + filename)
+
+#=========================================================================
+parser = argparse.ArgumentParser()
+parser.add_argument('--directory', help='Directory to search')
+args = parser.parse_args()
+directory = args.directory
+print(directory)
 
 gui = myTk()
 Win = gui.win('Search Pi Notes', '640x480')
