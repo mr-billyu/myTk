0667b78a7f2f94e0323903519e399cc8337585ea
mr-billyu <linux9window0@yahoo.com>
2019-01-29 13:20:41 -0500

    Added main():

--- dirgrep.py
+++ dirgrep.py
@@ -5,9 +5,7 @@ from myTk import myTk
 import os
 import re
 
-
 def get_entry(event='x'):
-    global directory
     gui.clearlist('results')
     param = gui.getentry('searchfor')
     option = '-n'
@@ -24,37 +22,42 @@ def get_entry(event='x'):
     gui.setlist('results', '======END======')
 
 def get_selected(event):
-    global directory 
     selection = gui.getlistselection('results').split(":", 2)
     filename = directory + '/' + selection[0]
     os.system('lxterminal -e less +' + selection[1] + ' ' + filename)
 
-#=========================================================================
-parser = argparse.ArgumentParser(description='GUI program that searches' +
-                                             ' all *.txt files in a' +
-                                             ' directory for entered' +
-                                             ' parameter.')
-parser.add_argument('directory', help='Directory to search')
-args = parser.parse_args()
-directory = args.directory
-print(directory)
+def main():
+    global gui
+    global directory
+    parser = argparse.ArgumentParser(
+                         description='GUI program that searches' +
+                                     ' all *.txt files in a' +
+                                     ' directory for entered' +
+                                     ' parameter.')
+    parser.add_argument('directory', help='Directory to search')
+    args = parser.parse_args()
+    directory = args.directory
+    print(directory)
 
-gui = myTk()
-Win = gui.win('Search Pi Notes', '640x480')
+    gui = myTk()
+    Win = gui.win('Search Pi Notes', '640x480')
 
-gui.frame('fill')
-menudata = (("file", ("quit", Win.quit)),)
-gui.menubar(menudata)
+    gui.frame('fill')
+    menudata = (("file", ("quit", Win.quit)),)
+    gui.menubar(menudata)
 
-gui.frame('fill')
-gui.textentry('searchfor', 'Find', 50)
-gui.bindentry('searchfor', '<Return>', get_entry)
-gui.focusentry('searchfor')
-gui.checkbutton('case', 'Case', get_entry)
-gui.checkbutton('word', 'Word', get_entry)
+    gui.frame('fill')
+    gui.textentry('searchfor', 'Find', 50)
+    gui.bindentry('searchfor', '<Return>', get_entry)
+    gui.focusentry('searchfor')
+    gui.checkbutton('case', 'Case', get_entry)
+    gui.checkbutton('word', 'Word', get_entry)
 
-gui.frame('expand')
-gui.listbox('results')
-gui.bindlist('results', '<Double-1>', get_selected)
+    gui.frame('expand')
+    gui.listbox('results')
+    gui.bindlist('results', '<Double-1>', get_selected)
 
-Win.mainloop()
+    Win.mainloop()
+
+if __name__ == '__main__':
+    main()
