70f39916eb0c1830ac51d0d3ed792f90375eebd3
mr-billyu <linux9window0@yahoo.com>
2019-02-26 14:03:48 -0500

    Woring message window.

--- myMsg.py
+++ myMsg.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 
 from myTk import myTk
+import argparse
+
+parser = argparse.ArgumentParser(description='Display the message on the' +
+                                             'command line in a window.')
+parser.add_argument('msg', help='Message to display.')
+args = parser.parse_args()
+message = args.msg
 
 def ok():
     msg.quit()
 
 msg = myTk()
-msg.win('Message', '640x100')
+msg.win('Message', '440x100')
 
-msg.frame('expand')
-msg.rotext('message')
-msg.insertrotext('message', '0.0', 'Test Message')
+f = msg.frame('')
+tb = msg.rotextbox('message')
+tb.config(width=60, height=4)
+msg.insertrotextbox('message', '0.0', message)
 
-msg.frame('fill')
-msg.button('Ok', 'tomato', ok)
+b = msg.button('Close', 'tomato', ok)
+b.pack_forget()
+b.pack(side='right')
 
 msg.root.mainloop()
 
